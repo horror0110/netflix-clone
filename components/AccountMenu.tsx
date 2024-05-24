e97eebe095e6d001f1e2051ad8d11393c5ad303d
ganaa.12396@yahoo.com <ganaa.12396@yahoo.com>
2024-05-24 11:58:24 +0800

    service

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -8,11 +8,13 @@ interface AccountMenuProps {
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+
+  const { data: session, status } = useSession();
   if (!visible) {
     return null;
   }
 
-  const { data: session, status } = useSession();
+
   return (
     <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-3">
